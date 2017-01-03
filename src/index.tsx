94a21eaace1b8e923a1695ec34d182d24ac8f0ba
Sergey Smirnov <ses@veeroute.com>
2017-01-03 05:24:41 +0300

    apply inspinia style.

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,18 @@ import * as ReactDOM from 'react-dom';
 import {IStore} from '~react-redux~redux';
 import {Provider} from 'react-redux';
 import configureStore from './app/store/configureStore';
-import {Router, browserHistory} from 'react-router';
+import {Router, hashHistory} from 'react-router';
 import routes from './app/config/routes';
-import 'todomvc-app-css/index.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/font-awesome/css/font-awesome.css';
+import '../node_modules/animate.css/animate.min.css';
 import './inspinia.css';
 
 const store: IStore<any> = configureStore({});
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={browserHistory}>{routes}</Router>
+    <Router history={hashHistory}>{routes}</Router>
   </Provider>,
   document.getElementById('root')
 );
