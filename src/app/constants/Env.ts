080ac132ecc4b5efd0e006c8b6d32b65dda531d0
Sergey Smirnov <s@slctd.com>
2017-02-25 20:53:58 +0300

    add test environment.

---
+++ src/app/constants/Env.ts
@@ -0,0 +1,9 @@
+export const Env = {
+  TEST: 'test',
+  DEVELOPMENT: 'development',
+  PRODUCTION: 'production',
+  CURRENT: this.TEST,
+  APP_ID: 'B6827009-0DB1-84F3-FF46-4977EAB2F000',
+  APP_KEY: 'E7DF33A5-965F-1BD3-FF7F-78A26737F200',
+  APP_VER: 'v1'
+};
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,16 +1,16 @@
 import * as Backendless from 'backendless';
 import Todo from '../models/todo';
+import {Env} from '../constants/Env';
+import guid from '../utils/guid';
 
 export default (function TodoService() {
-
-    const APP_ID = 'B6827009-0DB1-84F3-FF46-4977EAB2F000';
-    const APP_KEY = 'E7DF33A5-965F-1BD3-FF7F-78A26737F200';
-    const APP_VER = 'v1';
-
-    Backendless.initApp(APP_ID, APP_KEY, APP_VER);
+    Backendless.initApp(Env.APP_ID, Env.APP_KEY, Env.APP_VER);
 
     return {
         saveTodo: function (todo: Todo): Object {
+            if (Env.CURRENT === Env.TEST) {
+              return Object.assign(todo, {objectId: guid()});
+            }
             try {
                 return Backendless.Persistence.of(Todo).save(todo);
             } catch (error) {
@@ -18,6 +18,9 @@ export default (function TodoService() {
             }
         },
         removeTodo: function (todo: Todo): Object {
+            if (Env.CURRENT === Env.TEST) {
+              return {};
+            }
             try {
                 return Backendless.Persistence.of(Todo).remove(todo);
             } catch (error) {
@@ -25,6 +28,9 @@ export default (function TodoService() {
             }
         },
         getTodos: function (): Todo[] {
+            if (Env.CURRENT === Env.TEST) {
+              return [];
+            }
             try {
                 let result = Backendless.Persistence.of(Todo).find();
                 // noinspection TypeScriptUnresolvedVariable
