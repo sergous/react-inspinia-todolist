38d3defac4630b2b3079ef12af33e67f1e297637
Sergey Smirnov <s@slctd.com>
2017-03-01 00:55:05 +0300

    dry todo service.

--- src/app/services/todo.service.tsx
+++ src/app/services/todo.service.tsx
@@ -3,47 +3,48 @@ import Todo from '../models/todo';
 import {Env} from '../constants/Env';
 import guid from '../utils/guid';
 
-export default (function TodoService() {
+class TodoService {
+  constructor() {
     Backendless.initApp(Env.APP_ID, Env.APP_KEY, Env.APP_VER);
+  }
+  saveTodo(todo: Todo): Todo {
+    if (Env.CURRENT === Env.TEST) {
+      try {
+        todo.objectId = todo.objectId || guid();
+        return todo;
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    try {
+      //noinspection TypeScriptValidateTypes
+      return Backendless.Persistence.of(Todo).save(todo);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  removeTodo(todo: Todo): Object {
+    if (Env.CURRENT === Env.TEST) {
+      return {};
+    }
+    try {
+      return Backendless.Persistence.of(Todo).remove(todo);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  getTodos(): Todo[] {
+    if (Env.CURRENT === Env.TEST) {
+      return [];
+    }
+    try {
+      let result = Backendless.Persistence.of(Todo).find();
+      // noinspection TypeScriptUnresolvedVariable
+      return result.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
 
-    return {
-        saveTodo: function (todo: Todo): Todo {
-            if (Env.CURRENT === Env.TEST) {
-               try {
-                 todo.objectId = todo.objectId || guid();
-                 return todo;
-               } catch (e) {
-                 console.log(e);
-               }
-            }
-            try {
-                // noinspection TypeScriptUnresolvedVariable
-                return Backendless.Persistence.of(Todo).save(todo).data;
-            } catch (error) {
-                console.log(error);
-            }
-        },
-        removeTodo: function (todo: Todo): Object {
-            if (Env.CURRENT === Env.TEST) {
-              return {};
-            }
-            try {
-                return Backendless.Persistence.of(Todo).remove(todo);
-            } catch (error) {
-                console.log(error);
-            }
-        },
-        getTodos: function (): Todo[] {
-            if (Env.CURRENT === Env.TEST) {
-              return [];
-            }
-            try {
-                let result = Backendless.Persistence.of(Todo).find();
-                // noinspection TypeScriptUnresolvedVariable
-                return result.data;
-            } catch (error) {
-                console.log(error);
-            }
-        }
-    };
-}());
+export default new TodoService();
