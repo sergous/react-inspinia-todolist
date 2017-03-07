1d05047e14376751c9b8e140fdb3824efc3cd2fc
Sergey Smirnov <s@slctd.com>
2017-03-08 01:33:44 +0300

    add editable text field.

--- src/app/components/ContactItem.tsx
+++ src/app/components/ContactItem.tsx
@@ -2,13 +2,13 @@
 
 import * as React from 'react';
 import Contact from '../models/contact';
+import EditableTextField from './EditableTextField';
+import {IEditableTextField} from './EditableTextField';
 
 interface IContactItemProps {
   contact: Contact;
-  // todo: edit contact
   editContact?: (contact: Contact) => void;
   deleteContact?: (contact: Contact) => void;
-  completeContact?: (contact: Contact) => void;
 }
 
 interface IContactItemState {
@@ -18,10 +18,8 @@ interface IContactItemState {
 class ContactItem extends React.Component<IContactItemProps, IContactItemState> {
   static propTypes = {
     contact: React.PropTypes.object.isRequired,
-    // todo: add actions
-    // editContact: React.PropTypes.func.isRequired,
-    // deleteContact: React.PropTypes.func.isRequired,
-    // completeContact: React.PropTypes.func.isRequired
+    editContact: React.PropTypes.func.isRequired,
+    deleteContact: React.PropTypes.func.isRequired,
   };
 
   constructor(props: any, context: any) {
@@ -29,22 +27,19 @@ class ContactItem extends React.Component<IContactItemProps, IContactItemState> 
     this.state = {
       editing: false
     };
-    this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
-    this.handleDoubleClick = this.handleDoubleClick.bind(this);
     this.handleSave = this.handleSave.bind(this);
-  }
-
-  handleChange() {
-    this.props.completeContact(this.props.contact);
+    this.handleSaveTextField = this.handleSaveTextField.bind(this);
   }
 
   handleClick() {
     this.props.deleteContact(this.props.contact);
   }
 
-  handleDoubleClick() {
-    this.setState({editing: true});
+  handleSaveTextField( textfield: IEditableTextField ) {
+    this.props.contact[textfield.name] = textfield.text;
+    this.props.editContact(this.props.contact);
+    this.setState({editing: false});
   }
 
   handleSave( contact: Contact ) {
@@ -54,50 +49,73 @@ class ContactItem extends React.Component<IContactItemProps, IContactItemState> 
       this.props.contact.name = contact.name;
       this.props.editContact(this.props.contact);
     }
-    this.setState({editing: false});
   }
 
   render() {
     const contact = this.props.contact;
 
-    let element;
-    if (this.state.editing) {
-      // todo: contact edit form
-      /*element = (
-        <ContactTextInput
-          name={contact.name}
-          editing={this.state.editing}
-          onSave={this.handleSave}
-          />
-      );*/
-    } else {
-      element = (
-        <div className='contact-box center-version'>
-          <a href=''>
-            <img alt={contact.name} className='img-circle' src={contact.photo_url} />
-            <h3 className='m-b-xs'>{contact.name}</h3>
-            {contact.position && <div className='font-bold'>{contact.position}</div>}
-            <address className='m-t-md'>
-              {contact.company && <strong>{contact.company}</strong>}
-              {contact.address1 && <p className='no-margins'>{contact.address1}</p>}
-              {contact.address2 && <p className='no-margins'>{contact.address2}</p>}
-              {contact.phone && <p className='no-margins'>
-                <abbr title='Phone'>P: </abbr><span>{contact.phone}</span></p>}
-            </address>
-          </a>
-          <div className='contact-box-footer'>
-            <div className='m-t-xs btn-group'>
-              {contact.phone && <a className='btn btn-xs btn-white' href={`tel:${contact.phone}`}>
-                <i className='fa fa-phone'></i> Call </a>}
-              {contact.email && <a className='btn btn-xs btn-white' href={`mailto:${contact.email}`}>
-                <i className='fa fa-envelope'></i> Email</a>}
-              {contact.twitter && <a className='btn btn-xs btn-white' href={`http://twitter.com/${contact.twitter}`}>
-                <i className='fa fa-user-plus'></i> Follow</a>}
-            </div>
+    let element = (
+      <div className='contact-box center-version'>
+        <a>
+          <img alt={contact.name} className='img-circle' src={contact.photo_url} />
+          <h3 className='m-b-xs'>
+            <EditableTextField
+              text={contact.name}
+              name='name'
+              onSave={this.handleSaveTextField}
+            />
+          </h3>
+          {contact.position && <div className='font-bold'>
+            <EditableTextField
+              text={contact.position}
+              name='position'
+              onSave={this.handleSaveTextField}
+            />
+          </div>}
+          <address className='m-t-md'>
+            {contact.company && <strong>
+              <EditableTextField
+                text={contact.company}
+                name='company'
+                onSave={this.handleSaveTextField}
+              />
+            </strong>}
+            {contact.address1 && <p className='no-margins'>
+              <EditableTextField
+                text={contact.address1}
+                name='address1'
+                onSave={this.handleSaveTextField}
+              />
+            </p>}
+            {contact.address2 && <p className='no-margins'>
+              <EditableTextField
+                text={contact.address2}
+                name='address2'
+                onSave={this.handleSaveTextField}
+              />
+            </p>}
+            {contact.phone && <p className='no-margins'>
+              <abbr title='Phone'>P: </abbr><span>
+              <EditableTextField
+                text={contact.phone}
+                name='phone'
+                onSave={this.handleSaveTextField}
+              />
+            </span></p>}
+          </address>
+        </a>
+        <div className='contact-box-footer'>
+          <div className='m-t-xs btn-group'>
+            {contact.phone && <a className='btn btn-xs btn-white' href={`tel:${contact.phone}`}>
+              <i className='fa fa-phone'></i> Call </a>}
+            {contact.email && <a className='btn btn-xs btn-white' href={`mailto:${contact.email}`}>
+              <i className='fa fa-envelope'></i> Email</a>}
+            {contact.twitter && <a className='btn btn-xs btn-white' href={`http://twitter.com/${contact.twitter}`}>
+              <i className='fa fa-user-plus'></i> Follow</a>}
           </div>
         </div>
-      );
-    }
+      </div>
+    );
 
     return (
       <div className='col-md-6' key={contact.objectId}>
---
+++ src/app/components/EditableTextField.tsx
@@ -0,0 +1,80 @@
+/// <reference path="../../../typings/index.d.ts" />
+
+import * as React from 'react';
+
+export interface IEditableTextField {
+  text: string;
+  name: string;
+}
+
+interface IEditableTextFieldProps {
+  onSave: ( textfield: IEditableTextField ) => void;
+  text: string;
+  name: string;
+  editing?: boolean;
+  placeholder?: string;
+}
+
+interface IEditableTextFieldState {
+  text: string;
+  editing?: boolean;
+}
+
+class EditableTextField extends React.Component<IEditableTextFieldProps, IEditableTextFieldState> {
+  static propTypes = {
+    onSave: React.PropTypes.func.isRequired,
+    text: React.PropTypes.string.isRequired,
+    name: React.PropTypes.string.isRequired,
+    editing: React.PropTypes.bool,
+    placeholder: React.PropTypes.string
+  };
+
+  constructor(props: any, context: any) {
+    super(props, context);
+    this.state = {
+      text: this.props.text || '',
+      editing: this.props.editing
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e: any) {
+    const text = e.target.value.trim();
+    if (e.which === 13) {
+      this.props.onSave( {text: text, name: this.props.name} );
+      this.setState({text: text, editing: !this.state.editing});
+    }
+  }
+
+  handleChange(e: any) {
+    this.setState({text: e.target.value});
+  }
+
+  handleClick(e: any) {
+    this.setState({text: this.state.text, editing: !this.state.editing});
+  }
+
+  render() {
+    return (
+      <span onClick={this.handleClick}>
+        {this.state.editing
+          ? <input
+              className='edit'
+              type='text'
+              name={this.props.name}
+              placeholder={this.props.placeholder}
+              autoFocus={true}
+              value={this.state.text}
+              onChange={this.handleChange}
+              onKeyDown={this.handleSubmit}
+            />
+          : this.state.text
+        }
+      </span>
+    );
+  }
+}
+
+export default EditableTextField;
